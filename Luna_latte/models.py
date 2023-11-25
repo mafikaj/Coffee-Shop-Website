8da6ed1b5dca16176ef3be1777e6bc498ec35d43
Mafika Nkosi <mafikaj,nkosi@gmail.com>
2023-11-25 20:37:23 +0200

    Add docstrings for Models in models.py

--- Luna_latte/models.py
+++ Luna_latte/models.py
@@ -1,14 +1,34 @@
-from django.db import models
-
-# Create your models here.
 # lunalatte/models.py
+from django.db import models
 
 
 class MenuItem(models.Model):
+    """
+    Represents a menu item in the application.
+
+    Attributes:
+    - name (str): The name of the menu item.
+    - description (str): The description of the menu item.
+    - price (float): The price of the menu item.
+    - image (str): The URL or path to the image of the menu item.
+
+    Methods:
+    - __str__: Returns a string representation of the menu item.
+    """
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
     image = models.CharField(max_length=2083)
 
     def __str__(self):
+        """
+        Returns a string representation of the menu item.
+
+        Returns:
+        - str: The name of the menu item.
+        """
         return self.name
+
+    class Meta:
+        app_label = 'Luna_latte'

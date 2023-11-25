a37e72400146e26a21f320fc03507a2fd433741b
Mafika Nkosi <mafikaj,nkosi@gmail.com>
2023-11-25 20:39:27 +0200

    Add docstrings for Views in views.py

--- Luna_latte/views.py
+++ Luna_latte/views.py
@@ -7,19 +7,45 @@ from django.shortcuts import render
 from .forms import ContactForm
 from django.http import HttpResponseRedirect
 
-# lunalatte/views.py
-
 
 def home(request):
+    """
+    Render the home page with a list of menu items.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - Rendered HTML page with a list of menu items.
+    """
     menu_items = MenuItem.objects.all()
     return render(request, 'home.html', {'menu_items': menu_items})
 
 
 def about(request):
+    """
+    Render the about page.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - Rendered HTML page for the about section.
+    """
     return render(request, 'about.html')
 
 
 def contact_view(request):
+    """
+    Render and handle the contact form.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - If the request method is POST and the form is valid, handle the form submission.
+    - Rendered HTML page with the contact form.
+    """
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -33,6 +59,16 @@ def contact_view(request):
 
 
 def signup(request):
+    """
+    Render and handle the user signup form.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - If the request method is POST and the form is valid, create a new user and log in.
+    - Rendered HTML page with the signup form.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -45,6 +81,16 @@ def signup(request):
 
 
 def login(request):
+    """
+    Render and handle the user login form.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - If the request method is POST and the form is valid, log in the user.
+    - Rendered HTML page with the login form.
+    """
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
@@ -56,5 +102,14 @@ def login(request):
 
 
 def logout(request):
+    """
+    Log out the user.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - Redirect to the home page after logging out the user.
+    """
     auth_logout(request)
     return redirect('home')
